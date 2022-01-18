f516ee1fc029a1192fd9d21ffb0b5cf804f2d49e
fadilasyifa <fadilasyifa5@gmail.com>
2022-01-18 08:33:16 +0700

    first comit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA_Compatiable" content="IE=edge">
+    <meta name="viewport" content="widht=device=width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"
+    <title>Fadila | About</title>
+</head>
+<body>
+    <h1> Halaman About </h1>
+    <h3> <?php echo $nama; ?> </h3>
+    <p> <?php echo $email; ?> </p>
+    <img src="images/<?= $gambar; ?>" alt="<?= $nama; ?>" width="200px">
+</body>
+</hmtl>
---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA_Compatiable" content="IE=edge">
+    <meta name="viewport" content="widht=device=width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"
+    <title>Fadila | Gallery</title>
+</head>
+<body>
+    <h1> Halaman Gallery </h1>
+</body>
+</hmtl>
