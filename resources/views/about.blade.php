ca516971c854e0baaeb3bdd0a54a118e67e01e9e
fadilasyifa <fadilasyifa5@gmail.com>
2022-02-14 21:46:04 +0700

    modul 4

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,5 @@
     <h1> Halaman About </h1>
     <h3> {{ $nama }} </h3>
     <h3> {{ $email }} </h3>
-    <img src="image/{{ $gambar }}" alt="{{ $nama }}" width="200px">
+    <img src="images/{{ $gambar }}" alt="{{ $nama }}" width="200px">
 @endsection
